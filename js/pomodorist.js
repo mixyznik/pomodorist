306d75dba08fccde8754d076672458354d6942db
mixyznik <mixy80@gmail.com>
2017-11-30 21:10:50 +0100

    my seccond

--- js/pomodorist.js
+++ js/pomodorist.js
@@ -1,1 +1,15 @@
+var countDownDate = new Date("Sep 5, 2018 15:37:25").getTime()
+var x = setInterval(function() {
+    var now = new Date();
+    var distance = countDownDate - now;
 
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("demo").innerHTML = minutes + "m " + seconds + "s ";
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000);
